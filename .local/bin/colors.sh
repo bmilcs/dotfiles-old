def65e7a23bebf50f259d01c6a3f461a14968d4c
bmilcs <bmilcs@yahoo.com>
2020-12-27 16:50:28 -0500

    Added /home/bmilcs/.local/bin/bootstrap

---
+++ .local/bin/colors.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+
+#export nord0 #2E3440
+#export nord1 #3B4252
+#export nord2 #434C5E
+#export nord3 #4C566A
+#export nord4 #D8DEE9
+#export nord5 #E5E9F0
+#export nord6 #ECEFF4
+#export nord7 #8FBCBB
+#export nord8 #88C0D0
+#export nord9 #81A1C1
+#export nord10 #5E81AC
+#export nord11 #BF616A
+#export nord12 #D08770
+#export nord13 #EBCB8B
+#export nord14 #A3BE8C
+#export nord15 #B48EAD
+
+export FOREGROUND="#B0B0B0"
+export BACKGROUND="#282828"
+#  BLACK + DARKGREY
+export COLOR0="#000000"
+export COLOR8="#222222"
+#  DARKRED + RED
+export COLOR1="#434343"
+export COLOR9="#434343"
+#  DARKGREEN + GREEN
+export COLOR2="#6B6B6B"
+export COLOR10="#A5A5A5"
+#  DARKYELLOW + YELLOW
+export COLOR3="#969696"
+export COLOR11="#E5E5E5"
+#  DARKBLUE + BLUE
+export COLOR4="#4A4A4A"
+export COLOR12="#4D4D4D"
+# DARKMANGENTA + MANGENTA
+export COLOR5="#707070"
+export COLOR13="#747474"
+# DARKCYAN + CYAN
+export COLOR6="#A9A9A9"
+export COLOR14="#C4C4C4"
+# LIGHTGREY + WHITE
+export COLOR7="#FFFFFF"
+export COLOR15="#DEDEDE"
+
