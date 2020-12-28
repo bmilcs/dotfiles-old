7a54f4e19c670e4f848ae45380a947258772a84c
bmilcs <bmilcs@yahoo.com>
2020-12-28 14:32:36 -0500

    update

--- .config/polybar/bspwm.sh
+++ .config/polybar/bspwm.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 # Terminate already running bar instances
 killall -q polybar
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -r -c ~/.config/polybar/bspwm.conf bspwm 2>/home/bmilcs/.config/polybar/bspwm.log &
+
+
+polybar -q -c ~/.config/polybar/bspwm.conf bspwm 2>/home/bmilcs/.config/polybar/bspwm.log &
 #polybar -c ~/.config/polybar/bspwm.conf -rq tray 2>/home/bmilcs/.config/polybar/log &
 #polybar -c ~/.config/polybar/bspwm.conf -rq title 2>/home/bmilcs/.config/polybar/log &
