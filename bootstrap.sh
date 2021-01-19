58f6e29f302ce5ec587bb1948de3a7af450ef089
bmilcs <bmilcs@yahoo.com>
2021-01-18 19:26:17 -0500

    cleaned up formatting, tweaked deb-vm

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,56 +5,95 @@
 #  ██▄▪▐███ ██▌▐█▌▐█▌▐█▌ ▄▐███▌▐█▄▪▐█  ═╩╝╚═╝ ╩ ╚  ╩╩═╝╚═╝╚═╝
 #  ·▀▀▀▀ ▀▀  █▪▀▀▀▀▀▀.▀▀▀ ·▀▀▀  ▀▀▀▀   https://dot.bmilcs.com
 #────────────────────────────────────────────────────────────
-#   dotfile repo bootstrap / deployment              
+#   DOTFILE REPO BOOTSTRAP / DEPLOYMENT              
 #────────────────────────────────────────────────────────────
-#     TODO 1. backup existing files
-#          2. for cfg in pwd, stow -R $cfg
-
+#   TODO 1. backup existing files
+#        2. for cfg in pwd, stow -R $cfg
+#────────────────────────────────────────────────────────────
 source ./bin/bin/_head
+
+# DISTRO CHECK
 
 if [ -f "/etc/arch-release" ]; then
   DISTRO='arch'; else DISTRO='debian'; fi
   _t bmilcs/bootstrap initiated for $DISTRO
 
-#
+#────────────────────────────────────────────────────────────
 # ARCHLINUX
-#
+#────────────────────────────────────────────────────────────
 
 if [[ ${DISTRO} == "arch" ]]; then
+
+  #
+  # STOW 
+  #
+
   for dir in ~/bm/*/ ; do
-#    if [[ ! $dir == *txt* ]] && [[ ! $dir == *asset* ]]; then
     if [[ ! $dir == *asset* ]]; then
       _i bootstraping $(basename $dir)...
       stow -R $(basename $dir)
       _s
     fi  
   done
-  _t bmilcs/bootstrap complete
+
+#────────────────────────────────────────────────────────────
+# DEBIAN
+#────────────────────────────────────────────────────────────
 
 else
 
-#
-# DEB-BASED
-#
+  #
+  # BASE PACKAGES
+  #
 
-_a checking for missing packages
-
+  _a checking for missing packages
   reqs=("curl" "zsh" "neovim" "git")
   dpkg -s "${reqs[@]}" >/dev/null 2>&1 || ( sudo apt-get install ${reqs[@]} && _s installed ${reqs[@]})
-
   _s all packages present
 
-  # zplugins
-  [[ -d ~/.zplugin ]] || ( _a zsh plugin setup started ; mkdir -p ~/.zplugin && git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin && _s completed zsh plugin setup )
-mkdir -p ~/.config/zsh
-  # vim-plug
-  [[ -f ~/.local/share/nvim/site/autoload/plug.vim ]] || (_a vim-plug setup started && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && _s completed vim-plug setup)
+  #
+  # ZSH
+  #
+
+  if [[ ! -d ~/.zplugin ]]; then 
+     _a zsh plugin setup started 
+     mkdir -p ~/.zplugin ~/.config/zsh
+     git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin 
+     _s completed zsh plugin setup
+  fi
+
+  #
+  # VIM 
+  #
+
+  if [[ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]]; then 
+    _a vim-plug setup started 
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 
+    _s completed vim-plug setup
+  fi
+
+  #
+  # STOW
+  #
 
   _a stowing necessary files
-  [[ -d ~/.zsh ]] || stow -R zsh && _s stowed zsh
-  [[ -d ~/.vim ]] || stow -R vim && _s stowed vim
-  stow -R git bin && _s stowed git, bin
+  stowee=("bin" "zsh" "vim" "git" "txt")
+  stow -R "${stowee[@]}" && _s ${stowee[@]} symlinked
+
+  #
+  # CHANGE SHELL
+  #
+
+  _ask swap to ZSH now?
+  if [[ $? == 0 ]]; then
+    chsh -s /usr/bin/zsh
+  else
+    _i ZSH skipped for now
+  fi
+
+
 fi
 
+_t bmilcs/bootstrap complete
 
