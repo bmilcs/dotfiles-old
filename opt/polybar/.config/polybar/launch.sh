ea656820bf305d729f859913c25b21d2c080751a
bmilcs <bmilcs@yahoo.com>
2021-02-20 10:58:13 +0000

    bmTP fixes

--- opt/polybar/.config/polybar/launch.sh
+++ opt/polybar/.config/polybar/launch.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #  ▄▄▄▄· • ▌ ▄ ·. ▪  ▄▄▌   ▄▄· .▄▄ ·   ──────────────────────
 #  ▐█ ▀█▪·██ ▐███▪██ ██•  ▐█ ▌▪▐█ ▀.   ╔╦╗╔═╗╔╦╗╔═╗╦╦  ╔═╗╔═╗
 #  ▐█▀▀█▄▐█ ▌▐▌▐█·▐█·██ ▪ ██ ▄▄▄▀▀▀█▄   ║║║ ║ ║ ╠╣ ║║  ║╣ ╚═╗
 #  ██▄▪▐███ ██▌▐█▌▐█▌▐█▌ ▄▐███▌▐█▄▪▐█  ═╩╝╚═╝ ╩ ╚  ╩╩═╝╚═╝╚═╝
 #  ·▀▀▀▀ ▀▀  █▪▀▀▀▀▀▀.▀▀▀ ·▀▀▀  ▀▀▀▀   https://dot.bmilcs.com
 #────────────────────────────────────────────────────────────
-#   POLYBAR LAUNCHEr             
+#   POLYBAR LAUNCHER             
 #────────────────────────────────────────────────────────────
 
+polybar-msg cmd quit >/dev/null 2>&1
 killall -q polybar
-# wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # pass polybar-script location as env var
 export PBS="$HOME/.config/polybar-scripts/polybar-scripts"
@@ -18,20 +19,16 @@ export PBS="$HOME/.config/polybar-scripts/polybar-scripts"
 if [[ $(pgrep -x "i3") ]]; then 
 
   # launch polybar, custom conf, forward output to log
-  set -x
-  polybar -c ~/.config/polybar/bars.ini -r i3 2>/home/bmilcs/.config/polybar/log &
-  polybar -c ~/.config/polybar/bars.ini -r dummy 2>/home/bmilcs/.config/polybar/log &
-  set +x
-
   echo ">>> i3 polybar launched..." >> ~/.config/polybar/log
+  polybar -l info -c ~/.config/polybar/bars.ini -r i3 2>/home/bmilcs/.config/polybar/log &
+  polybar -l info -c ~/.config/polybar/bars.ini -r dummy 2>/home/bmilcs/.config/polybar/log &
 
 elif [[ $(pgrep -x "bspwm") ]]; then
 
   # launch polybar, custom conf, forward output to log
-  set -x
-  polybar -c ~/.config/polybar/bars.ini bspwm 2>/home/bmilcs/.config/polybar/log &
-  set +x
-  echo ">>> bspwm polybar launched..." >> ~/.config/polybar/log
+  # echo ">>> bspwm polybar launched..." >> ~/.config/polybar/log
+  polybar -l trace -c ~/.config/polybar/bars.ini bspwm # 2>/home/bmilcs/.config/polybar/log &
+
 
 else
 
