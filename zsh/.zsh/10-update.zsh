b036d057b51a3d0616ef4564bb7b78e9a1f0ca1e
bmilcs <bmilcs@yahoo.com>
2021-03-03 19:31:29 -0500

    added zinit plugin update

--- zsh/.zsh/10-update.zsh
+++ zsh/.zsh/10-update.zsh
@@ -16,6 +16,7 @@ if [[ -f $ustatus ]]; then ulast="$(cat "$ustatus")" ; else
 if [[ ! "$unow" == "$ulast" ]]; then
   source $HOME/bin/_head ; _t bmilcs daily update 
   _a "last ran" ; _i "$DAYOFWEEK, $ulast of $MONTH"
-  _t "zinit self-update" && echo && zinit self-update && echo &&_s "\n" 
+  _t "zinit: self update" && echo && zinit self-update && echo &&_s "\n" 
+  _t "zinit: plugins update" && echo && zinit update && echo &&_s "\n" 
   up && echo "$unow" > "$ustatus" 
 fi
