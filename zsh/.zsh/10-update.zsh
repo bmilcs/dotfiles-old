9008d3e3a35c94d6cd8ca6076e8d42c1a1db047f
bmilcs <bmilcs@yahoo.com>
2021-03-16 13:19:02 -0400

    added yn prompt, simplified upvim code

--- zsh/.zsh/10-update.zsh
+++ zsh/.zsh/10-update.zsh
@@ -30,29 +30,39 @@ zstatus="$HOME/.config/up/zsh.bm"
 [[ -f $vstatus ]] && vlast="$(cat "$vstatus")" || vlast=00
 [[ -f $zstatus ]] && zlast="$(cat "$zstatus")" || zlast=00
 
-# zsh plugins & manager
-[[ ! "$today" == "$zlast" ]] \
-  && _a "zinit" && zinit self-update && _s \
-  && _a "plugins" && zinit update --all && _s \
-  && echo "$today" > "$zstatus"
+unset outdated
+for i in $dlast $flast $rlast $slast $vlast $zlast; do
+  [[ ! $today == "$i" ]] && outdated=1 && break
+done
 
-# fzf update
-[[ ! "$today" == "$flast" ]] && upfzf && echo "$today" > "$fstatus"
+if [[ -n $outdated ]]; then
+  if _askb "run daily update?"; then
 
-# system (pacman & aur)
-[[ ! "$today" == "$slast" ]] && up && echo "$today" > "$ustatus"
+    # zsh: plugins w/ manager
+    [[ ! "$today" == "$zlast" ]] \
+      && _a "zinit" && zinit self-update && _s \
+      && _a "plugins" && zinit update --all && _s \
+      && echo "$today" > "$zstatus"
 
-# github repo software
-if [[ ! "$today" == "$rlast" ]] && [[ -f /usr/local/bin/upr ]]; then
-  upr && echo "$today" > "$rstatus"
+    # packages: pacman & aur
+    [[ ! "$today" == "$slast" ]] && up && echo "$today" > "$ustatus"
+
+    # fzf
+    [[ ! "$today" == "$flast" ]] && upfzf && echo "$today" > "$fstatus"
+
+    # vim plug install & update  / vim +'PlugInstall --sync' +qall &> /dev/null
+    [[ ! "$today" == "$vlast" ]] && upvim && echo "$today" > "$vstatus"
+
+    # cloned repos
+    if [[ ! "$today" == "$rlast" ]] && [[ -f /usr/local/bin/upr ]]; then
+      upr && echo "$today" > "$rstatus"
+    fi
+
+    # dotfiles repo
+    if [[ ! "$HOST" == "bm"* ]] || [[ ! "$today" == "$dlast" ]]; then 
+      gp && echo "$today" > "$dstatus"
+    fi
+  else
+    _sb "skipped for now"
+  fi
 fi
-
-# dotfiles repo
-[[ ! "$today" == "$dlast" ]] && gp && echo "$today" > "$dstatus"
-
-# dotfiles every time (vm's)
-[[ ! "$HOST" == "bm"* ]] && gp
-
-# vim plug install & update  / vim +'PlugInstall --sync' +qall &> /dev/null
-[[ ! "$today" == "$vlast" ]] && upvim && echo "$today" > "$vstatus"
-
