8ab3a548c4b0ba4832eea5f6985e45c71b6bec6d
bmilcs <bmilcs@yahoo.com>
2021-06-05 12:51:58 -0400

    beet & pip autocomp

--- zsh/.config/zsh/10-autocomp.zsh
+++ zsh/.config/zsh/10-autocomp.zsh
@@ -1,12 +1,22 @@
-_i3()
-{
-  _script_commands=$(/path/to/your/script.sh shortlist)
+eval "`pip completion --zsh`"
+compctl -K _pip_completion pip3
 
-  local cur
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+_get_comp_words_by_ref() { :; }
+compopt() { :; }
+_filedir() { :; }
+eval "$(beet completion)"
 
-  return 0
-}
-complete -o nospace -F _script ./admin.sh
+#────────────────────────────────────────────────────────────  example  ───────
+
+# _i3()
+# {
+#   _script_commands=$(/path/to/your/script.sh shortlist)
+# 
+#   local cur
+#   COMPREPLY=()
+#   cur="${COMP_WORDS[COMP_CWORD]}"
+#   COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+# 
+#   return 0
+# }
+# complete -o nospace -F _script ./admin.sh
