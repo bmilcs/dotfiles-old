84da50e785f961a25998bd3114fd4ec8fde63736
bmilcs <bmilcs@yahoo.com>
2021-06-06 12:45:39 -0400

    overhaul, pid lock fixed & conditionals improved, vm background dotfiles & sourcing complete

--- zsh/.config/zsh/05-update.sh
+++ zsh/.config/zsh/05-update.sh
@@ -10,6 +10,7 @@
 # dotfile rc file debugging
 . ~/bin/sys/dotfile_logger && dotlog '+ $ZDOTDIR/05-update.sh'
 
+# bmilcs lib
 source _bm
 
 #───────────────────────────────────────────────────────────────  vars  ───────
@@ -28,89 +29,106 @@ ustatus="$shpath/system.bm"
 vstatus="$shpath/vim.bm"
 zstatus="$shpath/zsh.bm"
 astatus="$shpath/ansible.bm"
-pid_file="$shpath/lock.pid"
+lockpid="$shpath/lock.pid"
 
-[ ! -d "$shpath" ] && mkdir -p "$shpath"
+#───────────────────────────────────────────────────────  process lock  ───────
 
-# duplicate process prevention
-exec 9>"$pid_file"
-if ! flock -n 9  ; then
+mkdir -p "$shpath"
+
+exec 9>"$lockpid"
+if ! flock -n 9; then
   _w "${B}auto-update${YLW} duplicate prevented"
    exit 1
 fi
 
 #──────────────────────────────────────────────────────  history check  ───────
 
+[[ -f $astatus ]] && alast="$(cat "$astatus")" || alast=00
 [[ -f $dstatus ]] && dlast="$(cat "$dstatus")" || dlast=00
 [[ -f $fstatus ]] && flast="$(cat "$fstatus")" || flast=00
 [[ -f $rstatus ]] && rlast="$(cat "$rstatus")" || rlast=00
-[[ -f $ustatus ]] && slast="$(cat "$ustatus")" || slast=00 
+[[ -f $ustatus ]] && ulast="$(cat "$ustatus")" || ulast=00 
 [[ -f $vstatus ]] && vlast="$(cat "$vstatus")" || vlast=00
 [[ -f $zstatus ]] && zlast="$(cat "$zstatus")" || zlast=00
-[[ -f $astatus ]] && alast="$(cat "$astatus")" || alast=00
 
-#──────────────────────────────────────────────────────────────  begin  ───────
+#──────────────────────────────────────────────────────────  functions  ───────
 
-# vm dotfiles background update
+upped() {
+  echo "today" > "$1"
+}
+
+#───────────────────────────────────────────────  dotfiles [bmPC|bmTP]  ───────
+
+if [[ ! "$today" == "$dlast" ]] && [[ "$HOST" == "bm"* ]]; then 
+  gp && upped "$dstatus"
+fi
+
+#────────────────────────────────────────────────────  package manager  ───────
+
+[[ ! "$today" == "$ulast" ]] \
+  && up \
+  && upped "$ustatus"
+
+if [[ "$today" == "$ulast" ]] && [[ -n $auto ]]; then
+  _a "update: up script"
+  _s completed today
+fi
+
+#────────────────────────────────────────────────────  repo collection  ───────
+
+if [[ -f /usr/local/bin/upr ]]; then
+  if [[ ! "$today" == "$rlast" ]]; then
+    upr && upped "$rstatus"
+  elif [[ -n $auto ]]; then
+    _a "update: repos"
+    _s completed today
+  fi
+else # vm's via ssh 
+  upped "$rstatus"
+fi
+
+#────────────────────────────────────────────────────────────────  fzf  ───────
+
+[[ ! "$today" == "$flast" ]] \
+  && upfzf \
+  && upped "$fstatus"
+
+
+#─────────────────────────────────────────────────  ansible unfriendly  ───────
+
+if [[ -z $ansible ]]; then
+
+#──────────────────────────────────────────────────────────────  zinit  ───────
+  [[ ! "$today" == "$zlast" ]] &&\
+    upped "$zstatus" &&\
+    _a "zinit: self update" && zinit self-update && _s &&\
+    _a "zinit: plugins" && zinit update --all && _s 
+
+#────────────────────────────────────────────────────────  vim plugins  ───────
+
+  if [[ ! "$today" == "$vlast" ]] && [[ -z $ansible ]]; then
+    upvim && upped "$vstatus"
+  fi
+
+fi # end of ansible-friendly updates
+
+#──────────────────────────────────────────────────────────  bmpc only  ───────
+
+if [[ "$HOST" == "bmPC" ]] && [[ ! "$today" == "$alast" ]]; then
+  upped "$astatus"
+  _a ansible-playbook: update
+  apb update.yml \
+    && _s "done"
+fi
+
+#──────────────────────────────────────────────────────────  vm's only  ───────
+
 if [[ ! "$HOST" == "bm"* ]]; then
   {
     gp &> /dev/null \
     && source ~/.config/zsh/.zshrc \
     && source ~/.profile
   } &
-  echo "$today" > "$dstatus"
+  upped "$dstatus"
 fi
 
-# packages
-[[ ! "$today" == "$slast" ]] \
-  && up \
-  && echo "$today" > "$ustatus"
-
-if [[ "$today" == "$slast" ]] && [[ -n $auto ]]; then
-  _a "update: up script"
-  _s completed today
-fi
-
-# repos
-if [[ -f /usr/local/bin/upr ]]; then
-  if [[ ! "$today" == "$rlast" ]]; then
-    upr && echo "$today" > "$rstatus"
-  elif [[ -n $auto ]]; then
-    _a "update: repos"
-    _s completed today
-  fi
-else # vm's via ssh 
-  echo "$today" > "$rstatus"
-fi
-
-# fzf
-[[ ! "$today" == "$flast" ]] && upfzf && echo "$today" > "$fstatus"
-
-# ansible-unfriendly updates
-if [[ -z $ansible ]]; then
-
-  # zinit
-  [[ ! "$today" == "$zlast" ]] &&\
-    echo "$today" > "$zstatus" &&\
-    _a "zinit: self update" && zinit self-update && _s &&\
-    _a "zinit: plugins" && zinit update --all && _s 
-
-  # vim plugins
-  if [[ ! "$today" == "$vlast" ]] && [[ -z $ansible ]]; then
-    upvim && echo "$today" > "$vstatus"
-  fi
-
-fi # end of ansible-friendly updates
-
-# dotfiles repo: pc/laptop
-if [[ ! "$today" == "$dlast" ]] && [[ "$HOST" == "bm"* ]]; then 
-  gp && echo "$today" > "$dstatus"
-fi
-
-# ansible [bmpc] 
-if [[ "$HOST" == "bmPC" ]] && [[ ! "$today" == "$alast" ]]; then
-  echo "$today" > "$astatus"
-  _a ansible-playbook: update
-  apb update.yml \
-    && _s "done"
-fi
