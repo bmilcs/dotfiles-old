ca0400bc8e4e436a2b9a23d51ae807b5d1f65952
bmilcs <bmilcs@yahoo.com>
2021-06-06 12:06:44 -0400

    pid lock success. testing dotfile repo update background process w/ re-sourcing of dotfiles

--- zsh/.config/zsh/05-update.sh
+++ zsh/.config/zsh/05-update.sh
@@ -28,38 +28,19 @@ ustatus="$shpath/system.bm"
 vstatus="$shpath/vim.bm"
 zstatus="$shpath/zsh.bm"
 astatus="$shpath/ansible.bm"
-#running="$shpath/running.bm"
 pid_file="$shpath/lock.pid"
 
 [ ! -d "$shpath" ] && mkdir -p "$shpath"
 
+# duplicate process prevention
 exec 9>"$pid_file"
 if ! flock -n 9  ; then
   _w "${B}auto-update${YLW} duplicate prevented"
    exit 1
 fi
-# this now runs under the lock until 9 is closed (it will be closed automatically when the script ends)
-
-# script running?
-#[[ -s $pid_file ]] && exit
-
-# no, create pid_file
-#echo $BASHPID > $pid_file
-
-#──────────────────────────────────────────────────────────────  traps  ───────
-
-#ctrlC() {
-#  echo && _w "${B}now exiting cleanly"
-#  rm -rf "$running" \
-#  && _s "done\n" 
-#  exit
-#  }
-
-#trap 'ctrlC' 0 1 2 3 15
 
 #──────────────────────────────────────────────────────  history check  ───────
 
-[[ -d ~/.config/up ]] || mkdir -p ~/.config/up
 [[ -f $dstatus ]] && dlast="$(cat "$dstatus")" || dlast=00
 [[ -f $fstatus ]] && flast="$(cat "$fstatus")" || flast=00
 [[ -f $rstatus ]] && rlast="$(cat "$rstatus")" || rlast=00
@@ -70,72 +51,66 @@ fi
 
 #──────────────────────────────────────────────────────────────  begin  ───────
 
-# process check
-#if [[ ! -f "$running" ]]; then
+# vm dotfiles background update
+if [[ ! "$HOST" == "bm"* ]]; then
+  {
+    gp &> /dev/null \
+    && source ~/.config/zsh/.zshrc \
+    && source ~/.profile
+  } &
+  echo "$today" > "$dstatus"
+fi
 
-# # create $running
-# touch "$running"
+# packages
+[[ ! "$today" == "$slast" ]] \
+  && up \
+  && echo "$today" > "$ustatus"
 
-  # force non-workstations to update repo everytime (vm's, etc)
-  if [[ ! "$HOST" == "bm"* ]]; then
-    gp &> /dev/null & 
-    echo "$today" > "$dstatus"
-  fi
-  
-  # packages
-  [[ ! "$today" == "$slast" ]] && up && echo "$today" > "$ustatus"
-  if [[ "$today" == "$slast" ]] && [[ -n $auto ]]; then
-    _a "update: up script"
+if [[ "$today" == "$slast" ]] && [[ -n $auto ]]; then
+  _a "update: up script"
+  _s completed today
+fi
+
+# repos
+if [[ -f /usr/local/bin/upr ]]; then
+  if [[ ! "$today" == "$rlast" ]]; then
+    upr && echo "$today" > "$rstatus"
+  elif [[ -n $auto ]]; then
+    _a "update: repos"
     _s completed today
   fi
+else # vm's via ssh 
+  echo "$today" > "$rstatus"
+fi
 
-  # repos
-  if [[ -f /usr/local/bin/upr ]]; then
-    if [[ ! "$today" == "$rlast" ]]; then
-      upr && echo "$today" > "$rstatus"
-    elif [[ -n $auto ]]; then
-      _a "update: repos"
-      _s completed today
-    fi
-  else # vm's via ssh 
-    echo "$today" > "$rstatus"
+# fzf
+[[ ! "$today" == "$flast" ]] && upfzf && echo "$today" > "$fstatus"
+
+# ansible-unfriendly updates
+if [[ -z $ansible ]]; then
+
+  # zinit
+  [[ ! "$today" == "$zlast" ]] &&\
+    echo "$today" > "$zstatus" &&\
+    _a "zinit: self update" && zinit self-update && _s &&\
+    _a "zinit: plugins" && zinit update --all && _s 
+
+  # vim plugins
+  if [[ ! "$today" == "$vlast" ]] && [[ -z $ansible ]]; then
+    upvim && echo "$today" > "$vstatus"
   fi
 
-  # fzf
-  [[ ! "$today" == "$flast" ]] && upfzf && echo "$today" > "$fstatus"
+fi # end of ansible-friendly updates
 
-  # ansible-unfriendly updates
-  if [[ -z $ansible ]]; then
+# dotfiles repo: pc/laptop
+if [[ ! "$today" == "$dlast" ]] && [[ "$HOST" == "bm"* ]]; then 
+  gp && echo "$today" > "$dstatus"
+fi
 
-    # zinit
-    [[ ! "$today" == "$zlast" ]] &&\
-      echo "$today" > "$zstatus" &&\
-      _a "zinit: self update" && zinit self-update && _s &&\
-      _a "zinit: plugins" && zinit update --all && _s 
-
-    # vim plugins
-    if [[ ! "$today" == "$vlast" ]] && [[ -z $ansible ]]; then
-      upvim && echo "$today" > "$vstatus"
-    fi
-
-  fi # end of ansible-friendly updates
-
-  # dotfiles repo: pc/laptop
-  if [[ ! "$today" == "$dlast" ]] && [[ "$HOST" == "bm"* ]]; then 
-    gp && echo "$today" > "$dstatus"
-  fi
-
-  # ansible [bmpc] 
-  if [[ "$HOST" == "bmPC" ]] && [[ ! "$today" == "$alast" ]]; then
-    echo "$today" > "$astatus"
-    _a ansible-playbook: update
-    apb update.yml \
-      && _s "done"
-  fi
-
-#else # update running already
-
-  #_wb "instance of ${B}auto-update${BLU} running in another terminal."
-
-#fi
-
+# ansible [bmpc] 
+if [[ "$HOST" == "bmPC" ]] && [[ ! "$today" == "$alast" ]]; then
+  echo "$today" > "$astatus"
+  _a ansible-playbook: update
+  apb update.yml \
+    && _s "done"
+fi
