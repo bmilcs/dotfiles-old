62daff9ef1a3551ed633b20d00e6b0c57c8385be
bmilcs <bmilcs@yahoo.com>
2021-07-05 11:13:01 -0400

    bmtp | bmpc conditional launch

--- opt/polybar/.config/polybar/launch.sh
+++ opt/polybar/.config/polybar/launch.sh
@@ -9,38 +9,62 @@
 # KILL PREVIOUS INSTANCES
 #────────────────────────────────────────────────────────────
 
+LOG() { echo "$*" >> ~/.config/polybar/log; }
+
+LOG -----------------------------------------------
+LOG polybar script initiated
+LOG -----------------------------------------------
+
+[ -z $HOST ] \
+  && HOST=$(cat /etc/hostname)
+
+LOG killing polybar
+
 polybar-msg cmd quit > /dev/null 2>&1
 killall -q polybar
 
-# note: disabled wait until killed
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#─────────────────────────────────────────────────────────────────  i3  ───────
 
-# i3 config (workstation)
 if [[ $(pgrep -x "i3") ]]; then
 
-  # launch polybar, custom conf, forward output to log
-  echo ">>> i3 polybar launching..." >> ~/.config/polybar/log
-  if [[ $HOST == bmPC ]]; then
-    polybar -l info -c ~/.config/polybar/bmPC.ini -r i3 | tee /home/bmilcs/.config/polybar/log &
-  elif [[ $HOST == bmTP ]]; then
-    polybar -l info -c ~/.config/polybar/bmTP.ini -r i3 | tee /home/bmilcs/.config/polybar/log &
-  else
-    echo "error"
-    exit 1
-  fi
-      
-# polybar -l info -c ~/.config/polybar/bars.ini -r dummy 2> /home/bmilcs/.config/polybar/log &
+  LOG "> wm:   i3" 
+  LOG "> host: $HOST"
 
-# bspwm config (laptop)
+  if [[ $HOST == "bmPC" ]]; then
+
+    LOG "> launching desktop pc config"
+
+    polybar -c ~/.config/polybar/bmPC.ini -r i3 2>  ~/.config/polybar/log &
+
+  elif [[ $HOST == "bmTP" ]]; then
+
+    LOG "    launching laptop config"
+
+    polybar -c ~/.config/polybar/bmTP.ini -r i3 2> ~/.config/polybar/log &
+
+  else
+
+    LOG "error: host not recognized"
+    LOG "host: $HOST"
+    exit 1
+
+  fi
+
+#──────────────────────────────────────────────────────────────  bspwm  ───────
+
 elif [[ $(pgrep -x "bspwm") ]]; then
 
-  # launch polybar, custom conf, forward output to log
-  # echo ">>> bspwm polybar launched..." >> ~/.config/polybar/log
-  polybar -l trace -c ~/.config/polybar/bars.ini bspwm # 2>/home/bmilcs/.config/polybar/log &
+  polybar -c ~/.config/polybar/bars.ini bspwm 2>/home/bmilcs/.config/polybar/log &
 
-# future wm's
 else
 
-  echo ">>> ERROR: NO DESKTOP_SESSION MATCHED" >> ~/.config/polybar/log
+  echo ">>> ERROR: NO WINDOW MANAGER FOUND" >> ~/.config/polybar/log
 
 fi
+
+#──────────────────────────────────────────────────────────────  notes  ───────
+
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# polybar -l info -c ~/.config/polybar/bars.ini -r dummy 2> /home/bmilcs/.config/polybar/log &
+
+# polybar debugging: -l info / -l trace 
